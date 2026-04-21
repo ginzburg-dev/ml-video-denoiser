5992b37553e193e50c89df62d33d61ee87d063e7
ginzburg-dev <ginzburgdd@gmail.com>
2026-04-21 03:01:53 +0100

    update infer colorspace test

--- scripts/infer_test_cs_spatial.sh
+++ scripts/infer_test_cs_spatial.sh
@@ -6,11 +6,11 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 TRAINING_DIR="$ROOT_DIR/training"
 
 CHECKPOINT_SPATIAL="${CHECKPOINT:-checkpoints/cs_test_linear_spatial/best.pth}"
-CHECKPOINT_TEMPORAL="${CHECKPOINT:-checkpoints/cs_test_linear_temporal/best.pth}"
+CHECKPOINT_TEMPORAL="${CHECKPOINT:-checkpoints/cs_test_linear_cascade/best.pth}"
 INPUT_TGB1004140_DIR="${INPUT_DIR:-$HOME/data/tgb_train/TGB_training/train_noisy/TGB1004140_mid}"
 INPUT_TGB0502050_DIR="${INPUT_DIR:-$HOME/data/tgb_train/TGB_training/val_noisy/TGB0502050_mid}"
 OUTPUT_SPATIAL_DIR="${OUTPUT_DIR:-checkpoints/cs_test_linear_spatial/infer_test}"
-OUTPUT_TEMPORAL_DIR="${OUTPUT_DIR:-checkpoints/cs_test_linear_temporal/infer_test}"
+OUTPUT_TEMPORAL_DIR="${OUTPUT_DIR:-checkpoints/cs_test_linear_cascade/infer_test}"
 
 cd "$TRAINING_DIR"
 
